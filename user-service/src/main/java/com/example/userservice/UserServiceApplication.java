5924afa7509aecc9ff83d035bff93716b41dc4b0
Hamid Bong Brahim <brahbong@gmail.com>
2025-06-03 13:31:35 +0100

    Update UserServiceApplication.java

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
-        System.out.println("bongjourrrrrrrr");
+        System.out.println("bongrrrrr");
         SpringApplication.run(UserServiceApplication.class, args);
     }
 }
