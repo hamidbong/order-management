966592e85f5552d801490c5b76623233affc60ac
hamidbong <117206678+hamidbong@users.noreply.github.com>
2025-06-09 14:57:51 +0100

    com

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
-        System.out.println("bonjfddsdtrtrrr");
+        System.out.println("bonjfddeeeeeeesdtrtrrr");
         SpringApplication.run(UserServiceApplication.class, args);
     }
 }
