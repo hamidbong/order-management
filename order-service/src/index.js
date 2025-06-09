4cc2fc4ed103a191bb648fd410cfc40c750d9e04
hamidbong <117206678+hamidbong@users.noreply.github.com>
2025-06-09 13:04:43 +0100

    new

--- order-service/src/index.js
+++ order-service/src/index.js
@@ -26,5 +26,5 @@ app.post('/orders', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`order-service listening attfsssssft http://localhost:${port}`);
+  console.log(`order-service listening attfsssft http://localhost:${port}`);
 });
