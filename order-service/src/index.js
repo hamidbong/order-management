a10ca1548dbc6b384f589eb6f67aa9e8f44a648e
hamidbong <117206678+hamidbong@users.noreply.github.com>
2025-06-09 14:39:38 +0100

    new update

--- order-service/src/index.js
+++ order-service/src/index.js
@@ -26,5 +26,5 @@ app.post('/orders', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`order-service listening attfdgttrfvgesssft http://localhost:${port}`);
+  console.log(`order-service listening attfdgttsft http://localhost:${port}`);
 });
--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
     public static void main(String[] args) {
-        System.out.println("bonjfddsssgrtgrddtrtrrr");
+        System.out.println("bonjfddsssgrtgferfrddtrtrrr");
         SpringApplication.run(UserServiceApplication.class, args);
     }
 }
